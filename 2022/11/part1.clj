d2fa63b7d33a11074d8110d5426f302246acf725
Prince of Devils <104875-attaboy@users.noreply.framagit.org>
2022-12-14 11:53:27 +0000

    Solved Day 11 Part 1 in Clojure

--- 2022/11/part1.clj
+++ 2022/11/part1.clj
@@ -1,6 +1,6 @@
 (ns part1
   (:require [clojure.string :as str]
-            [clojure.repl :as repl]))
+            [clojure.stacktrace]))
 
 (defn parse-false-monkey
   [monkey-line]
@@ -16,7 +16,7 @@
 
 (defn parse-operation
   [monkey-line]
-  (let [parts (re-find #"Operation: new = old (.) (\d|\w)" monkey-line)
+  (let [parts (re-find #"Operation: new = old (.) (\d+|\w+)" monkey-line)
         op (second parts)
         amount (last parts)]
     {:op op :amount amount}))
@@ -25,9 +25,10 @@
   [monkey-line]
   (letfn [(parseInts [coll] (map #(Integer/parseInt %) coll))]
     (-> monkey-line
-      (str/replace "  Starting items: " "")
-      (str/split #", ")
-      (parseInts))))
+        (str/replace "  Starting items: " "")
+        (str/split #", ")
+        (parseInts)
+        vec)))
 
 ;; TODO: Trampoline or similar?
 (defn parse-monkey
@@ -38,40 +39,74 @@
         test-num (parse-test-num (nth monkey-str 3))
         true-monkey (parse-true-monkey (nth monkey-str 4))
         false-monkey (parse-false-monkey (nth monkey-str 5))]
-    {:items items :operation operation :test-num test-num :true-monkey true-monkey :false-monkey false-monkey}))
+    {:items items :operation operation :test-num test-num :true-monkey true-monkey :false-monkey false-monkey :inspects 0}))
 
-(def monkeys (map parse-monkey (str/split (slurp "test.txt") #"\n\n")))
+(map parse-monkey (str/split (slurp "test.txt") #"\n\n"))
 
 (def operation-map {"*" *, "+" +})
 
-(defn move-to-monkey
-  [worry-level divisor true-m false-m]
-  (if (mod worry-level divisor)
-    true
-    false))
-
 (defn do-op
-  [item {:keys [op num]}]
-  (let [num (try
-             (Integer/parseInt num)
-             (catch Exception _ item))]
-    ((get operation-map op) item num)))
+  [item {:keys [op amount]}]
+  (let [amount (try
+                 (Integer/parseInt amount)
+                 (catch NumberFormatException _ item))]
+    ((get operation-map op) item amount)))
 
 (defn calculate-worry-level
   [item operation]
   (-> item
-    (do-op operation)
-    (/ 3)
-    (Math/floor)))
+      (do-op operation)
+      (/ 3)
+      (Math/floor)))
+
+(defn test-monkey
+  [new-item test-num true-monkey false-monkey]
+  (if (= 0.0 (mod new-item test-num))
+    true-monkey
+    false-monkey))
 
 (defn act-turn
-  [monkey]
-  (let [item (first (:items monkey))
-        operation (:operation monkey)
-        divisor (:test-num monkey)
-        true-m (:true-monkey monkey)
-        false-m (:false-monkey monkey)]
-    (calculate-worry-level item operation)))
+  [monkeys monkey-idx]
+  (let [{:keys [items operation test-num true-monkey false-monkey]} (nth monkeys monkey-idx)
+        new-item (calculate-worry-level (first items) operation)
+        to-monkey (test-monkey new-item test-num true-monkey false-monkey)]
+    (-> monkeys
+        (update-in [to-monkey :items] #(conj % new-item))
+        (update-in [monkey-idx :items] #(vec (rest %)))
+        (update-in [monkey-idx :inspects] inc))))
 
-(for [i (range 20)]
-  (map act-turn monkeys))
+(defn do-turns
+  [monkeys monkey-idx]
+  (let [monkey (get monkeys monkey-idx)]
+    (loop [monkeys monkeys items (:items monkey)]
+      (if (nil? (first items))
+        monkeys
+        ; TODO: We don't really need the items here...
+        (recur (act-turn monkeys monkey-idx) (rest items))))))
+
+(defn do-round
+  [monkeys]
+  (loop [monkeys monkeys monkey-idx 0]
+    (if (= (count monkeys) monkey-idx)
+      monkeys
+      (recur (do-turns monkeys monkey-idx) (inc monkey-idx)))))
+
+(defn play-stay-away
+  [monkeys rounds]
+  (loop [monkeys monkeys round 0]
+    (if (= rounds round)
+      monkeys
+      (recur (do-round monkeys) (inc round)))))
+
+(try
+  (play-stay-away (vec (map parse-monkey (str/split (slurp "test.txt") #"\n\n"))) 20)
+  (catch Exception e (clojure.stacktrace/print-stack-trace e)))
+
+(let [final-monkeys (play-stay-away (vec (map parse-monkey (str/split (slurp "input.txt") #"\n\n"))) 20)]
+  (->> final-monkeys
+       (map #(:inspects %))
+       (sort >)
+       (take 2)
+       (reduce *)))
+
+;; Result: 119715
