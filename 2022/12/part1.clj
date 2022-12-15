7fb2c070f0d21c8caeca32e092cf75448cb617c9
Attaboy <104875-attaboy@users.noreply.framagit.org>
2022-12-15 21:14:27 +0100

    I have no idea

--- 2022/12/part1.clj
+++ 2022/12/part1.clj
@@ -1,56 +1,86 @@
 (ns part1
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.stacktrace]))
 
 (defn find-start-pos
-  [field]
-  (let [max-y (count field)
-        max-x (count (first field))]
+  [fields]
+  (let [max-y (count fields)
+        max-x (count (first fields))]
     (loop [x 0 y 0]
       (cond
         (>= x max-x)
         (recur 0 (inc y))
         (>= y max-y)
         nil
-        (= \S (nth (nth field y) x))
+        (= \S (nth (nth fields y) x))
         [x y]
         :else (recur (inc x) y)))))
 
 ;(find-start-pos (map seq (str/split-lines (slurp "test.txt"))))
 ;(find-start-pos (map seq (str/split-lines (slurp "input.txt"))))
 
-(defn neighbours
-  "Returns neighbours in order up, down, left, right"
-  [field [posx posy]]
-  (apply merge
-         (for [[x y] '([0 -1] [0 +1] [-1 0] [+1 0])]
-           (let [x (+ posx x)
-                 y (+ posy y)
-                 val (nth (nth field y nil) x nil)]
-             {val [x y]}))))
+(defn index
+  [fields [x y]]
+  (nth (nth fields y nil) x nil))
+
+(defn get-neighbours
+  [fields [posx posy]]
+  (for [[x y] '([0 -1] [0 +1] [-1 0] [+1 0])]
+    (let [x (+ posx x)
+          y (+ posy y)
+          val (index fields [x y])]
+      {:pos [x y] :val val})))
 
 ;(neighbours (map seq (str/split-lines (slurp "test.txt"))) [3 3])
 ;(neighbours (map seq (str/split-lines (slurp "test.txt"))) [7 5])
 
-(defn index
-  [field [x y]]
-  (nth (nth field y) x))
-
 (defn height-difference
-  [field pos1 pos2]
-  (let [pos1 (int (index field pos1))
-        pos2 (int (index field pos2))]
-    (Math/abs (- pos2 pos1))))
+  [h1 h2]
+  (let [h1 (int h1)
+        h2 (int h2)]
+    (Math/abs (- h2 h1))))
 
 ;(height-difference (map seq (str/split-lines (slurp "test.txt"))) [0 0] [0 1])
 ;(height-difference (map seq (str/split-lines (slurp "test.txt"))) [0 1] [0 0])
 
-(defn find-path
-  [field start-pos]
-  (let [ns (neighbours field start-pos)]
+(defn path-contains?
+  [path pos]
+  (some #(= (:pos pos) (:pos %)) path))
+
+; (nil? (path-contains? '({:pos [0 1] :val \a} {:pos [0 2] :val \b}) {:pos [0 3] :val \c}))
+
+(defn find-allowed-fields
+  [fields path pos]
+  (let [ns (get-neighbours fields pos)]
     (->> ns
-         (filter seq)
-         (filter #(>= 1 (height-difference field start-pos (val %)))))))
+         (filter #(not (nil? (:val %))))
+         (filter #(>= 1 (height-difference \a (:val %))))
+         (filter #(nil? (path-contains? path %))))))
 
-(find-path (map seq (str/split-lines (slurp "test.txt"))) [0 0])
+;(find-allowed-fields (map seq (str/split-lines (slurp "test.txt"))) '() [0 0])
+;(find-allowed-fields (map seq (str/split-lines (slurp "test.txt"))) '() [2 4])
+;(find-allowed-fields (map seq (str/split-lines (slurp "test.txt"))) '({:pos [0 1], :val \a}) [0 0])
 
-(first (first {\i [7 4], nil [8 5]}))
+(defn follow-path
+  "Follows down a path, taking the path walked so far as input.
+   Returns the final path, either ending with \\E or nil."
+  [fields path]
+  (let [current (first path)
+        neighbours (find-allowed-fields fields path current)]
+    (if (nil? current)
+      path
+      (follow-path fields (conj path (first neighbours))))))
+
+(try
+ (let [fields (map seq (str/split-lines (slurp "test.txt")))
+       start (find-start-pos fields)]
+  (follow-path fields (list start)))
+ (catch Exception e (clojure.stacktrace/print-stack-trace e)))
+
+(defn find-path
+  [fields start]
+  ())
+
+(let [fields (map seq (str/split-lines (slurp "test.txt")))
+      start (find-start-pos fields)]
+  (find-path fields start))
