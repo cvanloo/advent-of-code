c7a5a9a7d4c21f544bb9156beb97998586ece16f
Colin van Loo <colin@vanloo.ch>
2023-12-22 13:03:55 +0100

    2023/05 clean up

--- 2023/05/part2b.clj
+++ 2023/05/part2b.clj
@@ -1,9 +1,11 @@
 (ns main-clj.core
   (:gen-class))
 
-
 (require '[clojure.string :as str]
-         '[instaparse.core :as insta])
+         '[instaparse.core :as insta]
+         'clojure.stacktrace)
+
+;; Parsing
 
 (def aoc-input
   (insta/parser
@@ -50,31 +52,26 @@
         seeds (parse-seeds seed-tokens)]
     (apply merge seeds (parse-mappings mappings-tokens))))
 
+;; Solving
 
+(defprotocol update-and-map
+  (update [thing fun])
+  (map-update [thing fun]))
 
-
-
-
-
-
+(deftype History [value history]
+  update-and-map
+  (update [h f]
+    (History.
+      (f (.value h))
+      (conj (.history h) (.value h))))
+  (map-update [h f]
+    (map
+      #(History. % (conj (.history h) (.value h)))
+      (f (.value h)))))
 
 (defn make-history
-  ([v]
-   {:result v
-    :history []})
-  ([v h]
-   {:result v
-    :history h}))
-
-(defn update-history
-  [{result :result history :history} f]
-  (make-history (f result)
-                (conj history result)))
-
-(defn map-update-history
-  [{result :result history :history} f]
-  (map #(make-history % (conj history result))
-       (f result)))
+  ([v] (History. v []))
+  ([v h] (History. v h)))
 
 (defmacro print-and-ret
   [v]
@@ -107,77 +104,129 @@
                      :temperature-to-humidity
                      :humidity-to-location])
 
+(defn prev-contains-next?
+  "
+    dst  dst-end
+     v    v
+  p: ■■■■■■
+  n:  ■■■
+      ^ ^
+    src src-end
+  "
+  [n p]
+  (and (< (dst p) (src n))
+       (> (dst-end p) (src-end n))))
 
+(defn prev-is-subset-of-next?
+  "
+      dst dst-end
+        v v
+  p:    ■■■    ■■■   ■■■       ■■■
+  n:  ■■■■■■   ■■■   ■■■■■   ■■■■■
+      ^    ^
+     src  src-end
+  "
+  [n p]
+  (and (>= (dst p) (src n))
+       (<= (dst-end p) (src-end n))))
 
+(defn prev-overlaps-next-end?
+  "
+    dst dst-end
+     v  v
+  p: ■■■■    ■■■■
+  n: ■■    ■■■■
+           ^  ^
+         src  src-end
+  "
+  [n p]
+  (and (>= (dst p) (src n))
+       (< (dst p) (src-end n))
+       (> (dst-end p) (src-end n))))
 
+(defn prev-overlaps-next-begin?
+  "
+    dst dst-end
+     v  v
+  p: ■■■■  ■■■■
+  n:   ■■    ■■■■
+             ^  ^
+            src src-end
+  "
+  [n p]
+  (and (> (src n) (dst p))
+       (< (src n) (dst-end p))
+       (>= (src-end n) (dst-end p))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(defn collapse-overlap
-  "Dear future me: I do not expect you to understand this. Sorry."
+(defn create-direct-mapping
+  "Creates a direct mapping from (src prev-map) to (dst next-map)."
   [prev-map next-map]
-  (letfn [(prev-contains-next? [n p]
-            (and (< (dst p) (src n))
-                 (> (dst-end p) (src-end n))))
-
-          (prev-is-subset-of-next? [n p]
-            (and (>= (dst p) (src n))
-                 (<= (dst-end p) (src-end n))))
-
-          (prev-overlaps-next-end? [n p]
-            (and (>= (dst p) (src n))
-                 (< (dst p) (src-end n))
-                 (> (dst-end p) (src-end n))))
-
-          (prev-overlaps-next-begin? [n p]
-            (and (> (src n) (dst p))
-                 (< (src n) (dst-end p))
-                 (>= (src-end n) (dst-end p))))]
-
-    (cond
-      (prev-contains-next? next-map prev-map)
+  (cond
+    (prev-contains-next? next-map prev-map)
+    ;   src        dst
+    ;    v          v
+    ; p: ■■■■■■ --> ■■■■■■
+    ; n:             ■■■   --> ■■■
+    ;            src ^     dst ^
+    ; =>  ■■■ ---------------> ■■■
+    (create-mapping
+      (dst next-map)
+      (+ (src prev-map) (- (src next-map) (dst prev-map)))
+      (len next-map))
+    
+    (prev-is-subset-of-next? next-map prev-map)
+    ;   src     dst
+    ;    v       v
+    ; p: ■■■ --> ■■■
+    ; n:        ■■■■■■ --> ■■■■■■
+    ;       src ^      dst ^
+    ; => ■■■ -------------> ■■■
+    (create-mapping
+      (+ (dst next-map) (- (dst prev-map) (src next-map)))
+      (src prev-map)
+      (len prev-map))
+    
+    (prev-overlaps-next-end? next-map prev-map)
+    ;   src       dst
+    ;    v         v
+    ; p: ■■■■■ --> ■■■■■
+    ; n:         ■■■■■ ----> ■■■■■
+    ;        src ^       dst ^   ^ dst-end
+    ; => ■■■ ----------------> ■■■
+    (let [overlap-len (- (len next-map) (- (dst prev-map) (src next-map)))]
+      (create-mapping
+        (- (dst-end next-map) overlap-len)
+        (src prev-map)
+        overlap-len))
+    
+    (prev-overlaps-next-begin? next-map prev-map)
+    ;   src       dst
+    ;    v         v
+    ; p: ■■■■■ --> ■■■■■
+    ; n:             ■■■■ --> ■■■■
+    ;            src ^    dst ^
+    ; =>   ■■■ -------------> ■■■
+    (let [before-len (- (src next-map) (dst prev-map))]
       (create-mapping
         (dst next-map)
-        (+ (src prev-map) (- (src next-map) (dst prev-map)))
-        (len next-map))
-      
-      (prev-is-subset-of-next? next-map prev-map)
-      (create-mapping
-        (+ (dst next-map) (- (dst prev-map) (src next-map)))
-        (src prev-map)
-        (len prev-map))
-      
-      (prev-overlaps-next-end? next-map prev-map)
-      (let [overlap-len (- (len next-map) (- (dst prev-map) (src next-map)))]
-        (create-mapping
-          (- (dst-end next-map) overlap-len)
-          (src prev-map)
-          overlap-len))
-      
-      (prev-overlaps-next-begin? next-map prev-map)
-      (let [before-len (- (src next-map) (dst prev-map))]
-        (create-mapping
-          (dst next-map)
-          (+ (src prev-map) before-len)
-          (- (len prev-map) before-len))))))
+        (+ (src prev-map) before-len)
+        (- (len prev-map) before-len)))))
 
-(defn resolve-between
-  [prev-map collapsed-mapping]
-  (let [collapsed-mapping (sort-by src collapsed-mapping)]
-    (loop [next (first collapsed-mapping)
-           overlaps (rest collapsed-mapping)
+(defn create-inbetween-mapping
+  "
+  Given a map `prev-map` and a list of mappings `overlap-maps` that
+  are all contained within `prev-map`, split `prev-map` into smaller
+  maps so that the overlapping parts from `prev-map` are removed.
+             p: ■■■■■■■■■■■■■■■■■
+             o:  ■ ■■   ■■■ ■ ■■
+             => ■ ■  ■■■   ■ ■  ■
+  The returned value is the resulting maps and the overlapping maps in a
+  sorted list.
+  "
+  [prev-map overlap-maps]
+  (let [overlap-maps (sort-by src overlap-maps)]
+    (loop [next (first overlap-maps)
+           overlaps (rest overlap-maps)
            total-len 0
            betweens []]
       (if (nil? next)
@@ -202,117 +251,49 @@
                 (create-mapping new-dst new-src new-len)
                 next))))))))
 
-
-      
-         
-
-(let [prev-map [50 10 100]
-      mapping [[25 50 20]
-               [5 70 30]
-               [125 100 50]]]
-  (map
-    (partial collapse-overlap prev-map)
-    mapping))
-
-(let [prev-map [50 10 100]
-      mapping [[25 52 16]
-               [5 70 27]
-               [125 101 20]]]
-  (map
-    (partial collapse-overlap prev-map)
-    mapping))
-
-; [50 10 2] *[25 12 16]* [68 28 2] *[5 30 27]* [97 57 4] *[125 61 20]* [121 81 29]
-(resolve-between
-  [50 10 100]
-  [[25 12 16]
-   [5 30 27]
-   [125 61 20]])
-; => [[50 10 2] [25 12 16] [68 28 2] [5 30 27] [97 57 4] [125 61 20] [121 81 29]]
-
-(resolve-between
-  [50 10 100]
-  [[25 10 20]
-   [5 30 30]
-   [125 60 50]])
-; => [[25 10 20] [5 30 30] [125 60 50]]
-
-(resolve-between
-  [50 10 100]
-  [])
-; => [[50 10 100]]
-
-
-
-(defn update-map-entry
-  "mapping-el can be a range that spans across / overlaps with multiple of the
-  ranges from map-data.
-  The ranges from map-data must not have any overlap with each other."
+(defn collapse-to-direct-mapping
+  "
+  Given a list of further mappings `map-data` and an initial mapping `mapping-el`,
+  create direct mappings from the initial source to the respective
+  destinations of the other mappings.
+  `mapping-el` can be a range that spans across / overlaps with multiple of
+  the ranges from `map-data`, but the ranges from `map-data` must not have any
+  overlap with each other.
+  "
   [map-data mapping-el]
   (->> map-data
-       (map (partial collapse-overlap mapping-el))
+       (map (partial create-direct-mapping mapping-el))
        (filter (comp not nil?))
-       (resolve-between mapping-el)))
-         
-
-(update-map-entry
-  [[25 50 20]
-   [5 70 30]
-   [125 100 50]]
-  [50 10 100])
-
-(update-map-entry
-  [[25 52 16]
-   [5 70 27]
-   [125 101 20]]
-  [50 10 100])
-
-(update-map-entry
-  [[70 205 5]
-   [80 357 3]]
-  [50 10 100])
-
-
-
+       (create-inbetween-mapping mapping-el)))
 
 (defn update-mapping
   [mappings map-data]
   (apply concat
-    (map (fn [m]
-           (map-update-history
-             m
-             (partial update-map-entry map-data)))
+    (map #(.map-update %
+            (partial collapse-to-direct-mapping map-data))
          mappings)))
-
-
-
-
-
-
-
 
 (defn collapse-mappings
   [mappings]
-  (let [collapsed-mapping (map make-history (map (partial apply map-self) (partition 2 (:seeds mappings))))]
-    (reduce update-mapping
-            collapsed-mapping
-            (map #(% mappings) traverse-order))))
+  (let [initial-mapping (map make-history (map (partial apply map-self) (partition 2 (:seeds mappings))))]
+    (reduce
+      update-mapping
+      initial-mapping
+      (map #(% mappings) traverse-order))))
 
-(def $input (slurp "sample.txt"))
-; => [46N 82N 10N] (the correct result)
+(comment (def $input (slurp "sample.txt")))
+(def $input (slurp "input.txt"))
 
-(comment (def $input (slurp "input.txt")))
-; => {:result [37806486N 1669061417N 30783317N], :history [[1499552892N 1499552892N 200291842N] [3654918290N 1499552892N 200291842N] [3855550220N 1669061417N 30783317N] [3855550220N 1669061417N 30783317N] [296168274N 1669061417N 30783317N] [860738727N 1669061417N 30783317N] [37806486N 1669061417N 30783317N]]}
-; => 37806486N is the correct result!
-
-(time (->> (parse-input $input)
+(try
+  (time
+    (.value
+      (->> (parse-input $input)
            collapse-mappings
-           (sort-by first)
-           first))
+           (sort-by #(.value %))
+           first)))
+  (catch Exception e
+    (clojure.stacktrace/print-stack-trace e)))
 
-
-
-(defn -main []
-  ())
-
-
+;              Loc Seed 
+; Test input: [46N 82N 10N]                     ("Elapsed time: 12.5392 msecs")
+; Real input: [37806486N 1669061417N 30783317N] ("Elapsed time: 111.0806 msecs")
